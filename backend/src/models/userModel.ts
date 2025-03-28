7ce7e264df297818e35e1d0bdc07ec17ea1d2b2d
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2025-03-28 15:08:09 -0700

    Feat #1: Create a user model

---
+++ backend/src/models/userModel.ts
@@ -0,0 +1,40 @@
+import { Schema, model, Document } from "mongoose";
+
+interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    collection: "Users",
+    timestamps: true,
+    strict: true,
+    validateBeforeSave: true,
+  }
+);
+
+const User = model<IUser>("User", userSchema);
+
+export default User;
