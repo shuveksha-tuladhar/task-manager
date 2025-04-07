234c4566ae3d7e361007e1a218cecee0e4cbafd4
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2025-04-07 10:42:42 -0700

    Feat #3: Update task list

--- frontend/src/components/TaskList.tsx
+++ frontend/src/components/TaskList.tsx
@@ -1,19 +1,22 @@
-import { useTaskStore } from "../stores/useTaskStores";
+import { useListStore } from "../stores/useListStores";
 import TaskItem from "./TaskItem";
+import { useTaskStore } from "../stores/useTaskStores";
 
 const TaskList: React.FC = () => {
-  const { tasks, activeCategory } = useTaskStore();
-  const filteredTasks = tasks.filter((task) => task.category === activeCategory);
+  const { activeList } = useListStore();
+  const { tasks } = useTaskStore();
+
+  const filteredTasks = tasks.filter((task) => task.listId === activeList?._id);
 
   return (
     <div className="p-4">
-      <h3 className="text-xl font-semibold mb-4">{activeCategory}</h3>
+      <h3 className="text-xl font-semibold mb-4">{activeList?.name}</h3>
       {filteredTasks.length === 0 ? (
         <p className="text-gray-500">No tasks in this category.</p>
       ) : (
         <ul className="space-y-3">
           {filteredTasks.map((task) => (
-            <TaskItem key={task.id} task={task} />
+            <TaskItem key={task._id} task={task} />
           ))}
         </ul>
       )}
