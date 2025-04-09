ce8d24a94bad9a5e83cfa53343af7ea75d55141a
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2025-04-09 14:32:23 -0700

    Feat #2: Add routes for task

--- backend/src/routes/taskRoutes.ts
+++ backend/src/routes/taskRoutes.ts
@@ -4,100 +4,132 @@ import authGuard from "../middleware/authGuard";
 
 const router = express.Router();
 
-router.post("/", authGuard, async (req: Request, res: Response): Promise<void> => {
-  try {
-    const { title, listId, userId, assignedToUserId, priority } = req.body;
+router.post(
+  "/",
+  authGuard,
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { title, listId, assignedToUserId, priority, isStarred } = req.body;
+      const userId = req.user?.userId;
+      const assignedToUser = assignedToUserId || req.user?.userId;
 
-    const newTask = new Task({
-      title,
-      listId,
-      userId,
-      assignedTo: [assignedToUserId],
-      priority: priority ?? "low",
-    });
+      const assignedToUserSet = new Set([userId, assignedToUser]);
 
-    await newTask.save();
-    res.status(201).json(newTask);
-  } catch (err: any) {
-    res.status(500).json({ message: err.message });
-  }
-});
+      const newTask = new Task({
+        title,
+        listId,
+        userId,
+        assignedTo: [...assignedToUserSet],
+        priority: priority ?? "low",
+        isStarred,
+      });
 
-router.get("/", authGuard, async (req: Request, res: Response): Promise<void> => {
-  try {
-    const tasks: ITask[] = await Task.find();
-    res.status(200).json(tasks);
-  } catch (err: any) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-router.get("/:id", authGuard, async (req: Request, res: Response): Promise<void> => {
-  try {
-    const task: ITask | null = await Task.findById(req.params.id);
-    res.status(200).json(task);
-  } catch (err: any) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-router.put("/:id", authGuard, async (req: Request, res: Response): Promise<void> => {
-  try {
-    const updatedTask: ITask | null = await Task.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true }
-    );
-
-    if (!updatedTask) {
-      res.status(404).json({ message: "Task not found" });
-      return;
+      await newTask.save();
+      res.status(201).json(newTask);
+    } catch (err: any) {
+      res.status(500).json({ message: err.message });
     }
-
-    res.status(200).json(updatedTask);
-  } catch (err: any) {
-    res.status(500).json({ message: err.message });
   }
-});
+);
 
-router.patch("/:id", authGuard, async (req: Request, res: Response): Promise<void> => {
-  try {
-    const updatedTask: ITask | null = await Task.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true }
-    );
-
-    if (!updatedTask) {
-      res.status(404).json({ message: "Task not found" });
-      return;
+router.get(
+  "/",
+  authGuard,
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const tasks: ITask[] = await Task.find({
+        $or: [{ userId: req.user?.userId }, { assignedTo: req.user?.userId }],
+      });
+      res.status(200).json(tasks);
+    } catch (err: any) {
+      res.status(500).json({ message: err.message });
     }
-
-    res.status(200).json(updatedTask);
-  } catch (err: any) {
-    res.status(500).json({ message: err.message });
   }
-});
+);
 
-router.delete("/:id", authGuard, async (req: Request, res: Response): Promise<void> => {
-  try {
-    const deletedTask: ITask | null = await Task.findByIdAndDelete(
-      req.params.id
-    );
-
-    if (!deletedTask) {
-      res.status(404).json({ message: "Task not found" });
-      return;
+router.get(
+  "/:id",
+  authGuard,
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const task: ITask | null = await Task.findById(req.params.id);
+      res.status(200).json(task);
+    } catch (err: any) {
+      res.status(500).json({ message: err.message });
     }
-
-    res.status(200).json({ message: "Task deleted" });
-  } catch (err: any) {
-    res.status(500).json({ message: err.message });
   }
-});
+);
+
+router.put(
+  "/:id",
+  authGuard,
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const updatedTask: ITask | null = await Task.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
+
+      if (!updatedTask) {
+        res.status(404).json({ message: "Task not found" });
+        return;
+      }
+
+      res.status(200).json(updatedTask);
+    } catch (err: any) {
+      res.status(500).json({ message: err.message });
+    }
+  }
+);
 
 router.patch(
-  "/:id/complete", authGuard,
+  "/:id",
+  authGuard,
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const updatedTask: ITask | null = await Task.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
+
+      if (!updatedTask) {
+        res.status(404).json({ message: "Task not found" });
+        return;
+      }
+
+      res.status(200).json(updatedTask);
+    } catch (err: any) {
+      res.status(500).json({ message: err.message });
+    }
+  }
+);
+
+router.delete(
+  "/:id",
+  authGuard,
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const deletedTask: ITask | null = await Task.findByIdAndDelete(
+        req.params.id
+      );
+
+      if (!deletedTask) {
+        res.status(404).json({ message: "Task not found" });
+        return;
+      }
+
+      res.status(200).json({ message: "Task deleted" });
+    } catch (err: any) {
+      res.status(500).json({ message: err.message });
+    }
+  }
+);
+
+router.patch(
+  "/:id/complete",
+  authGuard,
   async (req: Request, res: Response): Promise<void> => {
     try {
       const task: ITask | null = await Task.findById(req.params.id);
